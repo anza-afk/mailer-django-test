6bcd29b26d1603493f963488eef428ec596d68d6
Anatoly Zapravadov <zapokami@gmail.com>
2022-11-17 23:04:22 +0300

    mailing done

--- mailer/mail_factory/tasks.py
+++ mailer/mail_factory/tasks.py
@@ -1,16 +1,17 @@
 import string
 
-from django.contrib.auth.models import User
-from django.utils.crypto import get_random_string
-
 from celery import shared_task
 
 from models import MailingList
 from datetime import datetime
+from celery.utils.log import get_task_logger
 
-@shared_task
+logger = get_task_logger(__name__)
+
+@shared_task(name='send_db_emails')
 def send_db_emails():
     # mailing_list = MailingList.objects.get(id=args[1])
-    mailing_list = MailingList.objects.filter(mailing_time__lte=datetime.now())
-    mailing_list.send()
-    return 'all mail for {} sent!'.format(mailing_list.subject).encode("UTF-8")
+    mailing_list = MailingList.objects.filter(sent=False).filter(mailing_time__lte=datetime.now())
+    for mail in mailing_list:
+        mail.send()
+        logger.info('all mail for {} sent!'.format(mail.subject).encode("UTF-8"))
---
+++ mailer/mailer/celerysettings.py
@@ -0,0 +1,9 @@
+from datetime import timedelta
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERYBEAT_SCHEDULE = {
+    "runs-every-10-seconds": {
+        "task": "send_db_emails",
+        "schedule": timedelta(seconds=5)
+    },
+}
